3dd1cefb49fed1b9ecfb19b8847575c86d055b5a
petya.mincheva@gmail.com <User@192.168.119.47>
2012-04-01 22:04:13 +0200

    premiere version ligne

--- src/genielogiciel/Ligne.java
+++ src/genielogiciel/Ligne.java
@@ -4,6 +4,8 @@
  */
 package genielogiciel;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author User
@@ -11,19 +13,16 @@ package genielogiciel;
 public class Ligne {
     
     private String nom;
-    private Station[] stations;
+    private ArrayList<Station> stations;
     private boolean[] perturbations;
     private Perturbation[] perturbationsListe;
     
     
-    public Ligne(String nom,Station[] station){
+    public Ligne(String nom,ArrayList station){
         this.nom=nom;
-        stations=new Station[station.length];
-        for (int i=0;i<station.length;i++){
-            this.stations[i]=station[i];
-        }
-        perturbations=new boolean[station.length];
-        for(int i=0;i<stations.length;i++){
+        stations = new ArrayList(station);
+        perturbations=new boolean[station.size()];
+        for(int i=0;i<stations.size();i++){
             perturbations[i]=false;
         }
         
@@ -32,7 +31,7 @@ public class Ligne {
             
             
             // a changer
-            perturbationsListe[i]=new Perturbation(stations);
+            perturbationsListe[i]=new Perturbation();
         }
     }
 
@@ -52,6 +51,11 @@ public class Ligne {
       
     }
 
+    
+    public int getStations(){
+        return stations.size();
+    }
+    
     public Perturbation[] getPerturbationsListe() {
         return perturbationsListe;
     }
@@ -67,23 +71,21 @@ public class Ligne {
         
     }
 
-    public Station[] getStations() {
-        return stations;
-    }
+  
 
     public Station getStation(int i){
-        return stations[i];
+        return stations.get(i);
     }
     
-    public void setStations(Station[] stations) {
-        for(int i=0;i<stations.length;i++)  this.stations[i] = stations[i];
+    public void setStations(ArrayList stations) {
+        for(int i=0;i<this.stations.size();i++)  this.stations.set(i,(Station)stations.get(i));
     }
     
     
     public String toString(){
         String res=  this.nom + " passe par les stations: ";
-        for (int i=0;i<this.getStations().length;i++){
-                 res +=  "\n" + "-" + i + "- " + this.stations[i].toString();
+        for (int i=0;i<this.stations.size();i++){
+                 res +=  "\n" + "-" + i + "- " + this.stations.get(i).toString();
          }
         System.out.println(res);
         return res;
