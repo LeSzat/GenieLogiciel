22e2345f052445c386fc094b060fdabb2ee0d425
petya.mincheva@gmail.com <User@192.168.119.47>
2012-04-28 17:23:44 +0200

    25 1421 sprint 2 -- recherche

--- test/MetroTest.java
+++ test/MetroTest.java
@@ -15,16 +15,34 @@ public class MetroTest extends TestCase{
     
     Metro m=new Metro();
     
-    public void testgetNombreLignes(){
-      int lignes=  m.getNombreLignes();
-        assertEquals(3,lignes);
+//    public void testgetNombreLignes(){
+//      int lignes=  m.getNombreLignes();
+//        assertEquals(3,lignes);
+//        
+//    }
+    
+    public void testgetMinDistance(){
+    //   double res= m.getMinDistance(m.getStation(6)).getDistance();
+       double res = m.getMinDistance(m.getStation(5)).getDistance();
+       
+        assertEquals(10.,res);
         
     }
     
-    public void testgetMinDistance(){
-       double res= m.getMinDistance(m.getStation(6)).getDistance();
-        assertEquals(5.,res);
+    
+    public void testgetAretesStation(){
         
+       Station s= new Station("S2",10,20);
+       s.setLigne(1);
+       Station ss=new Station("S3",10,30);
+       ss.setLigne(1);
+      Arete a=new Arete("S2-S3",1,s,ss);
+      
+      
+      Arete res = m.getAretesStation(m.getStation(5)).get(0);
+      assertEquals(a.getSommetDepart().getNom(),res.getSommetDepart().getNom());
+      //assertEquals(a.getSommetArrivee(),res.getSommetArrivee());
+      
     }
 
     
--- test/ParcoursTest.java
+++ test/ParcoursTest.java
@@ -7,25 +7,61 @@ public class ParcoursTest extends TestCase{
     
     Metro m=new Metro();
     
-    public void testdijkstra(){
-        Parcours p=new Parcours(m.getStation(0),m.getStation(7));
-        ArrayList a=p.dijkstra();
-       // int lignes=  m.getNombreLignes();
-        ArrayList res=new ArrayList();
+//    public void testdijkstra(){
+//        Parcours p=new Parcours(m.getStation(0),m.getStation(7));
+//        ArrayList a=p.dijkstra();
+//       // int lignes=  m.getNombreLignes();
+//        ArrayList res=new ArrayList();
+//         Station s= new Station("S2",200,230);
+//       Station ss=new Station("S3",220,250);
+//       Station sss=new Station("S4",260,270);
+//       // Station ss2=new Station("SS3",30,40);
+//       //Station sss3=new Station("SS4",40,50);
+//        Station ssss=new Station("SS4",60,50);
+//        
+//        res.add(new Arete("S2-S3",1,s,ss));
+//        res.add(new Arete("S3-S4",1,ss,sss));
+//        res.add(new Arete("S4-SS4",2,sss,ssss));
+//        int chn=p.getCorrespondance();
+//      //  for(int i=0;i<res.size();i++){
+//            assertEquals(res,a);
+//         //   assertEquals(2,chn);
+//       // }
+//    }
+    
+    public void testdijkstraParPoint(){
+                ArrayList res=new ArrayList();
          Station s= new Station("S2",200,230);
        Station ss=new Station("S3",220,250);
        Station sss=new Station("S4",260,270);
+       Station ss2= new Station("SS2",50,15);
+       ss= new Station("SS2",50,15);
        // Station ss2=new Station("SS3",30,40);
        //Station sss3=new Station("SS4",40,50);
-        Station ssss=new Station("SS4",60,50);
-        
+        Station ssss=new Station("SS4",60,50);      
         res.add(new Arete("S2-S3",1,s,ss));
         res.add(new Arete("S3-S4",1,ss,sss));
-        res.add(new Arete("S4-SS4",2,sss,ssss));
-      //  for(int i=0;i<res.size();i++){
-            assertEquals(res,a);
-       // }
+        res.add(new Arete("S4-SS2",2,sss,ss2));
+        res.add(new Arete("S4-SS2",2,ss2,ss));
+        res.add(new Arete("S4-SS4",2,sss,ssss));       
+        
+        m.creerReseau();
+        Parcours p=new Parcours(m.getStation(0),m.getStation(7));
+         ArrayList res2=p.dijkstraParPoint(m.getStation(5));       
+        assertEquals(res,res2);
+        //assertEquals(2,p.getCorrespondance());
+             
     }
+    
+    
+//    public void testdijkstraChangements(){
+//         Parcours p=new Parcours(m.getStation(0),m.getStation(7));
+//         Station ss2=new Station("SS3",50,20);
+//         Station s= p.dijkstraChangements();
+//         ss2.setLigne(2);
+//         
+//        assertEquals(ss2,s);
+//    }
     
     
     public static void assertEquals(ArrayList o,ArrayList r){
