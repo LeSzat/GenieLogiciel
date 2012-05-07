c65b4f5803381bcae6389ff36c544af3770d744a
Mathias <mathias.szatmari@gmail.com>
2012-05-07 17:55:53 +0200

    finition classe ligne

--- src/genielogiciel/Ligne.java
+++ src/genielogiciel/Ligne.java
@@ -8,71 +8,76 @@ import java.util.ArrayList;
 import java.util.Iterator;
 
 /**
- *
- * @author User
+ *Modélise une ligne du métro
  */
 public class Ligne {
-    
-    
-    private int num;
-    private String nom;
-    public static double vitesse=18;   //vitesse moy 18m/s
 
+    /**
+     * Numéro de la ligne
+     */
+    private int num;
+    /**
+     * Nom de la ligne
+     */
+    private String nom;
+    /**
+     * Vi
+     */
+    public static final double vitesse = 18; 
+
+    /**
+     * Constructeur d'une ligne
+     * @param num Numero de la ligne
+     * @param nom Nom de la ligne
+     */
     public Ligne(int num, String nom) {
         this.num = num;
         this.nom = nom;
-//        this.m= new Metro();
-    }
-    
-    
-    public Ligne(){
-        this.num=0;
-        this.nom=" ";
     }
 
+    /**
+     * 
+     * @return le temps total du parcours de la ligne
+     */
+    public int getTempsTotalParcours() {
+        Metro m = new Metro();
+        double distance = 0;
+        int temps, index = 0, attente = 0;
+        ArrayList<Station> stations = m.getStationsLigne(this.num);
+        Iterator it = stations.iterator();
+        while (it.hasNext() && (index < stations.size() - 1)) {
+            Station s = (Station) it.next();
+            Station s2 = (Station) it.next();
+            distance = s.getDistance(s2);
+            index += 2;
+            attente += s2.getTempsArret();
+        }
+        distance = (distance / 1000) * (Ligne.vitesse);
+        temps = (int) (distance * 60) + attente;
+        return temps;
+    }
+
+    /**
+     * 
+     * @return Le nom de la ligne
+     */
     public String getNom() {
         return nom;
     }
 
-    public void setNom(String nom) {
-        this.nom = nom;
-    }
-
+    /**
+     * 
+     * @return le numero de la ligne
+     */
     public int getNum() {
         return num;
     }
 
+    /**
+     * Réaffecte le numéro de la ligne
+     * @param num numero de la ligne
+     */
     public void setNum(int num) {
         this.num = num;
     }
-    
-    
-    
-    /*
-     * renvoie le temps qu'il faut pour parcourir toutes les stations de la ligne (de terminus à terminus)
-     */
-    public int getTempsTotalParcours(){
-       Metro m=new Metro();
-        double distance=0;
-        int temps=0,index=0,attente=0;
-        ArrayList<Station> stations = m.getStationsLigne(this.num);
-        Iterator it= stations.iterator();
-        while(it.hasNext() && (index<stations.size()-1)){
-            Station s= (Station)it.next();
-            Station s2= (Station)it.next();
-            distance= s.getDistance(s2);
-            index +=2;
-            attente += s2.getTempsArret();
-        }
-        
-        
-        distance = (distance/1000) * (Ligne.vitesse);         
-        temps = (int)(distance*60) + attente;
-
-        return temps;
-    }
-    
-    
-    
- 
 }
