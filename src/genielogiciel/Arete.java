346e8efb701a1a1b40f2e98c72066c109b0a4fe5
petya.mincheva@gmail.com <User@192.168.119.47>
2012-05-07 17:35:54 +0200

    données fichier choix 1,2,3,6,9

--- src/genielogiciel/Arete.java
+++ src/genielogiciel/Arete.java
@@ -5,18 +5,43 @@
 package genielogiciel;
 
 /**
- *
- * @author User
+ * Classe qui modélise les liens entre deux stations d'une ligne
  */
 public class Arete implements java.lang.Comparable {
 
+    /**
+     * Nom de la ligne
+     */
     private String nom;
-    private int num;                // numéro de la ligne
+    /**
+     * Numéro de la ligne
+     */
+    private int num;
+    /**
+     * Station de départ
+     */
     private Station sommetDepart;
+    /**
+     * Station d'arrivée
+     */
     private Station sommetArrivee;
-    private double distance;      //la distance de parcours
+    /**
+     * Distance entre la station départ et la station d'arrivée en mètre
+     */
+    private double distance;
+    /**
+     * vrai si il y a une perturbation sur ce morceau de la ligne
+     */
     private boolean accident;
 
+    /**
+     * Constructeur d'une arete
+     *
+     * @param nom Nom de la ligne
+     * @param num Numero de la ligne
+     * @param depart Station de départ
+     * @param arrivee Station d'arrivée
+     */
     public Arete(String nom, int num, Station depart, Station arrivee) {
         this.nom = nom;
         this.num = num;
@@ -26,8 +51,12 @@ public class Arete implements java.lang.Comparable {
         this.accident = false;
     }
 
-    
-    
+    /**
+     * Rédéfintion de la fonction compareTo pour pouvoir comparer 2 aretes
+     *
+     * @param o Arete à comparer
+     * @return
+     */
     @Override
     public int compareTo(Object o) {
         double dist = ((Arete) o).getDistance();
@@ -40,60 +69,87 @@ public class Arete implements java.lang.Comparable {
         }
     }
 
+    /**
+     * Retourne la distance de cette arete
+     * @return la distance entre l'arrivée et le départ
+     */
     public double getDistance() {
         return distance;
     }
 
+    /**
+     * Affecte la distance entre 2 stations
+     * @param distance la distance entre les deux stations
+     */
     public void setDistance(double distance) {
         this.distance = distance;
     }
+ 
 
+    /**
+     *
+     * @return La station d'arrivée
+     */
     public Station getSommetArrivee() {
         return sommetArrivee;
     }
 
-    public void setSommetArrivee(Station sommetArrivee) {
-        this.sommetArrivee = sommetArrivee;
-    }
-
+    /**
+     *
+     * @return La station de départ
+     */
     public Station getSommetDepart() {
         return sommetDepart;
     }
 
-    public void setSommetDepart(Station sommetDepart) {
-        this.sommetDepart = sommetDepart;
-    }
-
+    /**
+     *
+     * @param s Station à comparer
+     * @return Vrai si cette station est la sation de départ
+     */
     public boolean isDepart(Station s) {
         return this.sommetDepart.getNom().equalsIgnoreCase(s.getNom());
     }
 
+    /**
+     *
+     * @param s Station à comparer
+     * @return Vrai si cette station est la sation d'arrivée
+     */
     public boolean isArrivee(Station s) {
         return this.sommetArrivee.getNom().equalsIgnoreCase(s.getNom());
     }
 
+    /**
+     * 
+     * @return Vrai s'il y a un accident sur cette arete
+     */
     public boolean isAccident() {
         return accident;
     }
 
+    /**
+     * Active ou désactive un accident
+     * @param accident Booléen vrai si accident/ faux sinon
+     */
     public void setAccident(boolean accident) {
         this.accident = accident;
     }
 
+    /**
+     * 
+     * @return Le nom de la ligne
+     */
     public String getNom() {
         return nom;
     }
 
-    public void setNom(String nom) {
-        this.nom = nom;
-    }
-
+   /**
+    * 
+    * @return Le numéro de la station
+    */
     public int getNum() {
         return num;
     }
 
-    public void setNum(int num) {
-        this.num = num;
-    }
-    
 }
--- src/genielogiciel/Horaire.java
+++ src/genielogiciel/Horaire.java
@@ -4,52 +4,57 @@
  */
 package genielogiciel;
 
-import java.sql.Time;
-
 /*
- * les horaires de passage des lignes(les mêmes pour toutes les lignes) toutes les 12min
+ * les horaires de passage des lignes(les mêmes pour toutes les lignes) toutes
+ * les 12min
  */
 public class Horaire {
-    public static int debut=6;      // l'heure du 1er passage
-    public static int fin=24;        // l'heure de passage du dernier
-    public static int[] horaire= {0,12,24,36,48};    // à quelle min passe la ligne
-    public int[] horaireRet;                         //à quelle min passe la ligne mais au retour
-   // private int ligne;
 
+    /**
+     * Heure du premier passage
+     */
+    public static final int debut = 6;
+    /**
+     * Heure du dernier passage
+     */
+    public static final int fin = 24;
+    /**
+     * Minute de chaque passage pour l'aller( identique pour chaqe heure)
+     */
+    public static final int[] horaire = {0, 12, 24, 36, 48};
+    /**
+     * Minute de chaque passage pour le retour( identique pour chaqe heure)
+     */
+    public int[] horaireRet;
+
+    /**
+     * Constructeur Horaire initilaise les horaires retours
+     */
     public Horaire() {
-      //  this.ligne = ligne;
-        this.horaireRet=new int[5];
-              
+        this.horaireRet = new int[5];
     }
 
-
-
+    /**
+     * 
+     * @return l'horaire du début de la ligne
+     */
     public static int getDebut() {
         return debut;
     }
 
-    public static void setDebut(int debut) {
-        Horaire.debut = debut;
-    }
-
+     /**
+     * 
+     * @return Le dernier horaire de la ligne
+     */
     public static int getFin() {
         return fin;
     }
 
-    public static void setFin(int fin) {
-        Horaire.fin = fin;
-    }
-
+    /**
+     * 
+     * @return La liste des horaires
+     */
     public static int[] getHoraire() {
         return horaire;
     }
-
-    public static void setHoraire(int[] horaire) {
-        Horaire.horaire = horaire;
-    }
-    
-    
-    
-    
-    
 }
